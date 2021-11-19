18e17ef0009a3928c3c545469b03dd14ae12f428
Matheus Eger <mateuseger098@gmail.com>
2021-11-19 16:20:33 -0300

    MudançasNoMain

---
+++ script.js
@@ -0,0 +1,31 @@
+/*TRANSIÇÃO DE BANNER*/
+
+let time = 5000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#banner img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
+
